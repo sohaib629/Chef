3b619c609ffd8b3eddeb48a3fe28dbefbf6b749d
vagrant <vagrant@localhost.localdomain>
2020-01-29 18:54:07 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+package 'git' do
+	action :install
+end
+
+file '/etc/motd' do
+	content 'This server is the property of ...'
+	action :create
+	owner  'vagrant'
+	group  'vagrant'
+end
+
+service 'ntpd' do
+	action [:enable, :start]
+end
