c3aace1d12cb5685fa0969bebebe30ea504c7fdd
“sohaib629” <sohaibanwar629@gmail.com>
2020-01-30 21:18:36 +0000

    Add host attributes into web server index.html through setup.rb

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -12,8 +12,17 @@ package 'git' do
 	action :install
 end
 
+#node ['ipaddress']
+#node ['memory']['total']
+
 file '/etc/motd' do
-	content 'This server is the property of ...'
+	content "This server is the property of ...
+	HOSTNAME: #{node['hostname']}
+	IPADDRESS: #{node['ipaddress']}
+	CPU: #{node['cpu']['0']['mhz']}
+	MEMORY: #{node['memory']['total']}
+"
+
 	action :create
 	owner  'vagrant'
 	group  'vagrant'
