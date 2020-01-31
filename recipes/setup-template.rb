bf0d317240d0b8d158bb0d02ac138138c7248a7f
“sohaib629” <sohaibanwar629@gmail.com>
2020-01-31 20:09:55 +0000

    Motd file creation through template

---
+++ recipes/setup-template.rb
@@ -0,0 +1,25 @@
+package 'vim-enhanced'
+package 'emacs'
+package 'nano'
+
+package 'tree' do
+	action :install
+end
+
+package 'ntp'
+
+package 'git' do
+	action :install
+end
+
+#node ['ipaddress']
+#node ['memory']['total']
+
+template '/etc/motd' do
+	source 'motd.erb'
+	action :create
+end
+
+service 'ntpd' do
+	action [:enable, :start]
+end
